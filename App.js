f7db2ae346a4d95d961e7973da4c83ca9e0fb7aa
IsakMakimartti <t2mais00@students.oamk.fi>
2024-03-26 14:29:14 +0200

    Added a component template for CalorieCalculator

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Mainmenu  from './components/Mainmenu'
+import CalorieCalculator from './components/CalorieCalculator';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Mainmenu/>
+   
+      <CalorieCalculator/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/CalorieCalculator.js
@@ -0,0 +1,11 @@
+import { useState, useEffect, React } from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import { Button } from 'react-native-paper';
+
+export default function CalorieCalculator() {
+  return (
+    <View>
+        <Text>Hei</Text>
+    </View>
+  )
+}
