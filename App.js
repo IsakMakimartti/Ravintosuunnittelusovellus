39c0e2ea0b2209e1784eced5c4679fb0c74dee19
IsakMakimartti <t2mais00@students.oamk.fi>
2024-03-26 15:15:31 +0200

    Added header, list and a button template for CalorieCalculator

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import CalorieCalculator from './components/CalorieCalculator';
 export default function App() {
   return (
     <View style={styles.container}>
-   
+      {/*<Mainmenu/>*/}
       <CalorieCalculator/>
       <StatusBar style="auto" />
     </View>
@@ -17,7 +17,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+   
     justifyContent: 'center',
   },
 });
--- components/CalorieCalculator.js
+++ components/CalorieCalculator.js
@@ -1,11 +1,88 @@
 import { useState, useEffect, React } from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import { StyleSheet, Text, View, FlatList, SafeAreaView } from "react-native";
 import { Button } from 'react-native-paper';
 
-export default function CalorieCalculator() {
-  return (
-    <View>
-        <Text>Hei</Text>
+// For testing
+const TESTDATA = [
+    {
+        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+        title: 'First Item',
+    },
+    {
+        id: '58694a0f-3da1-471f-bd96-145571e29d72',
+        title: 'Second Item',
+    },
+    {
+        id: '58694b0f-3da1-471f-bd96-145571e29d72',
+        title: 'Third Item',
+    },
+    {
+        id: '58694c0f-3da1-471f-bd96-145571e29d72',
+        title: 'Fourth Item',
+    },
+]
+
+// For testing
+const header = "Total calories"
+
+// For testing
+const Item = ({ title }) => (
+    <View style={styles.item}>
+        <Text style={styles.title}>{title}</Text>
     </View>
-  )
+);
+
+export default function CalorieCalculator() {
+    const [calories, setCalories] = useState(0)
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.headerItem}>
+                <Text style={styles.title}>{header}</Text>
+            </View>
+            <FlatList
+                data={TESTDATA}
+                renderItem={({ item }) => <Item title={item.title} />}
+                keyExtractor={item => item.id}
+            />
+            <Button
+                textColor="#000000"
+                buttonColor="#D3D3D3"
+                style={styles.button}
+                icon="plus" mode="contained"
+                onPress={() => ('Button pressed at CalorieCalculator')}>
+                Add
+            </Button>
+        </SafeAreaView>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 96,
+        marginHorizontal: 8
+    },
+    headerItem: {
+        backgroundColor: '#D3D3D3',
+        padding: 20,
+        marginBottom: 32,
+        marginHorizontal: 0
+    },
+    item: {
+        backgroundColor: '#D3D3D3',
+        padding: 20,
+        marginVertical: 4,
+        marginHorizontal: 0
+    },
+    title: {
+        fontSize: 32,
+        textAlign: 'center',
+        color: '#000000'
+    },
+    button: {
+        marginBottom: 96,
+        padding: 8,
+        marginHorizontal: 16
+    }
+});
