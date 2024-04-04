65eeaf1482bfc507455eee90156bfd256dc1d331
Isak Mäkimartti <t2mais00@students.oamk.fi>
2024-04-04 13:13:25 +0300

    Caloricalculator added to main application

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Searchpage from './components/Searchpage'
-import Mainmenu  from './components/Mainmenu'
+import Mainmenu from './components/Mainmenu'
 import Recipebuilder from './components/Recipebuilder';
-import RecipePage  from './components/RecipePage'
+import RecipePage from './components/RecipePage'
 import Footer from './components/Footer'
+import CalorieCalculator from './components/CalorieCalculator';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 
@@ -12,48 +13,56 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <View style={styles.container}>
-    <NavigationContainer>
-    <Stack.Navigator 
-      screenOptions={{
-        headerTintColor: 'black',
-        headerStyle: {backgroundColor: '#c5ee7d'},
-        headerTitleStyle: {
-          fontSize: 40
-        },
-        headerTitleAlign: 'center',
-        title: "RSS",
-      }}
-    >
-    <Stack.Screen
-          name="Home"
-          component={Mainmenu}
-        />
-        <Stack.Screen
-          name="Recipe"
-          component={RecipePage}
-          options={({ route }) => ({
-            title: "Recipe", 
-            headerTitle: route.RecipePage, 
-          })}
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerTintColor: 'black',
+            headerStyle: { backgroundColor: '#c5ee7d' },
+            headerTitleStyle: {
+              fontSize: 40
+            },
+            headerTitleAlign: 'center',
+            title: "RSS",
+          }}
+        >
+          <Stack.Screen
+            name="Home"
+            component={Mainmenu}
           />
-      <Stack.Screen
-          name="Search"
-          component={Searchpage}
-          options={({ route }) => ({
-            title: "Search", 
-            headerTitle: route.Searchpage, 
-          })}
-        />
-               <Stack.Screen
-          name="Recipebuilder"
-          component={Recipebuilder}
-          options={({ route }) => ({
-            title: "Recipe Builder", 
-            headerTitle: route.Recipebuilder, 
-          })}
-        />
-      </Stack.Navigator>
-         <Footer/>
+          <Stack.Screen
+            name="Recipe"
+            component={RecipePage}
+            options={({ route }) => ({
+              title: "Recipe",
+              headerTitle: route.RecipePage,
+            })}
+          />
+          <Stack.Screen
+            name="Search"
+            component={Searchpage}
+            options={({ route }) => ({
+              title: "Search",
+              headerTitle: route.Searchpage,
+            })}
+          />
+          <Stack.Screen
+            name="Recipebuilder"
+            component={Recipebuilder}
+            options={({ route }) => ({
+              title: "Recipe Builder",
+              headerTitle: route.Recipebuilder,
+            })}
+          />
+          <Stack.Screen
+            name="CalorieCalculator"
+            component={CalorieCalculator}
+            options={({ route }) => ({
+              title: "CalorieCalculator",
+              headerTitle: route.CalorieCalculator,
+            })}
+          />
+        </Stack.Navigator>
+        <Footer />
       </NavigationContainer>
       <StatusBar style="auto" />
     </View>
@@ -62,7 +71,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
+    flex: 1,
     backgroundColor: '#fff',
   },
 });
